19e36a59023326024785abae19b9f3fc52a5fcf1
андрей пользователь <you@example.com>
2022-05-01 14:31:22 +0300

    transformArray

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,23 +1,68 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Create transformed array based on the control sequences that original
  * array contains
- * 
+ *
  * @param {Array} arr initial array
  * @returns {Array} transformed array
- * 
+ *
  * @example
- * 
+ *
  * transform([1, 2, 3, '--double-next', 4, 5]) => [1, 2, 3, 4, 4, 5]
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
- * 
+ *
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+
+function transform(arr) {
+  const x = new Error("'arr' parameter must be an instance of the Array!");
+
+  if (arr == []) {
+    return arr;
+  } else if (Array.isArray(arr)) {
+    let a = [];
+
+    for (let i = 0; i < arr.length; i++) {
+      let el = arr[i];
+      if (el == "--double-next") {
+        if (arr[arr.length - 1] !== el) {
+          a.push(arr[i + 1]);
+        }
+      } else if (el == "--discard-prev") {
+        a.pop();
+      } else if (el == "--double-prev") {
+        if (arr[0] !== el) {
+          a.push(arr[i - 1]);
+        }
+      } else if (el == "--discard-next") {
+        i++;
+      } else {
+        a.push(arr[i]);
+      }
+    }
+
+    return a;
+
+    // console.log("arr :>> ", arr);
+    // console.log("a :>> ", a);
+  } else {
+    // console.log("x :> ", x);
+    return x;
+  }
 }
 
+// transform([1, 2, 3, "--discard-next", 1337, "--discard-prev", 4, 5]);
+// transform(["--discard-prev", 4, 5]);
+// transform([1, 2, 3, "--discard-next", 4, 5]);
+// transform(["--discard-prev", 1, 2, 3]);
+// transform(["--double-prev", 1, 2, 3]);
+// transform([1, 2, 3, "--double-next"]);
+// transform([1, 2, 3, "--discard-next"]);
+// transform(98789);
+// transform([1, 2, 3, "--discard-next", 1337, "--double-prev", 4, 5]);
+// transform([1, 2, 3, "--double-next", 1337, "--double-prev", 4, 5]);
+// transform([1, 2, 3, "--discard-next", 1337, "--discard-prev", 4, 5]);
+// transform([1, 2, 3, "--double-next", 1337, "--discard-prev", 4, 5]);
 module.exports = {
-  transform
+  transform,
 };
--- src/what-season.js
+++ src/what-season.js
@@ -1,21 +1,24 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Extract season from given date and expose the enemy scout!
- * 
+ *
  * @param {Date | FakeDate} date real or fake date
  * @returns {String} time of the year
- * 
+ *
  * @example
- * 
+ *
  * getSeason(new Date(2020, 02, 31)) => 'spring'
- * 
+ *
  */
-function getSeason(/* date */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getSeason(date) {
+  // if (typeof date == Date) {
+  //   console.log("1 :>> ", 1);
+  // }
+  console.log(" :>> ", typeof date);
 }
+getSeason(new Date(83, 9, 25, 16, 20, 23, 544));
 
 module.exports = {
-  getSeason
+  getSeason,
 };
