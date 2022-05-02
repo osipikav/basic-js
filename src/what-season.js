a620360572a68ea8ac55e8887c6f80cd3c48692d
андрей пользователь <you@example.com>
2022-05-02 13:21:00 +0300

    feat: what-season

--- src/what-season.js
+++ src/what-season.js
@@ -11,13 +11,36 @@ const { NotImplementedError } = require("../extensions/index.js");
  * getSeason(new Date(2020, 02, 31)) => 'spring'
  *
  */
+
 function getSeason(date) {
-  // if (typeof date == Date) {
-  //   console.log("1 :>> ", 1);
-  // }
-  console.log(" :>> ", typeof date);
+  if (date == undefined) {
+    return "Unable to determine the time of year!";
+  }
+  if (!(date instanceof Date) || Object.getOwnPropertyNames(date).length > 0)
+    throw new Error("Invalid date!");
+  d = date.getMonth();
+
+  switch (d.toString()) {
+    case "11":
+    case "0":
+    case "1":
+      return "winter";
+    case "2":
+    case "3":
+    case "4":
+      return "spring";
+    case "5":
+    case "6":
+    case "7":
+      return "summer";
+    case "8":
+    case "9":
+    case "10":
+      return "autumn";
+    default:
+      return "Invalid date!";
+  }
 }
-getSeason(new Date(83, 9, 25, 16, 20, 23, 544));
 
 module.exports = {
   getSeason,
