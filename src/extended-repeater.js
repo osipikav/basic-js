4d9a7f924b9e3a39f363f4b9602b8862ba7283b4
андрей пользователь <you@example.com>
2022-05-03 01:54:46 +0300

    feat: extended-repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,25 +1,74 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Create a repeating string based on the given parameters
- *  
+ *
  * @param {String} str string to repeat
- * @param {Object} options options object 
+ * @param {Object} options options object
  * @return {String} repeating string
- * 
+ *
  *
  * @example
- * 
- * repeater('STRING', { repeatTimes: 3, separator: '**', 
+ *
+ * repeater('STRING', { repeatTimes: 3, separator: '**',
  * addition: 'PLUS', additionRepeatTimes: 3, additionSeparator: '00' })
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+
+function repeater(str, options) {
+  let optionsDefolt = {
+    repeatTimes: 1,
+    separator: "+",
+    addition: "",
+    additionRepeatTimes: 0,
+    additionSeparator: "|",
+  };
+  let repeatTimes = options.repeatTimes;
+  let separator = options.separator;
+  let addition = options.addition;
+  let additionRepeatTimes = options.additionRepeatTimes;
+  let additionSeparator = options.additionSeparator;
+
+  if (options.repeatTimes == undefined) {
+    repeatTimes = optionsDefolt.repeatTimes;
+  }
+  // console.log("repeatTimes :>> ", repeatTimes);
+  if (options.separator == undefined) {
+    separator = optionsDefolt.separator;
+  }
+  // console.log("separator :>> ", separator);
+  if (options.addition == undefined) {
+    addition = optionsDefolt.addition;
+  }
+  // console.log("addition :>> ", addition);
+  if (options.additionRepeatTimes == undefined) {
+    additionRepeatTimes = optionsDefolt.additionRepeatTimes;
+  }
+  // console.log("additionRepeatTimes :>> ", additionRepeatTimes);
+  if (options.additionSeparator == undefined) {
+    additionSeparator = optionsDefolt.additionSeparator;
+  }
+  // console.log("additionSeparator :>> ", additionSeparator);
+
+  let arr = [];
+
+  for (let y = 0; y < repeatTimes; y++) {
+    arr.push(str);
+    arr.push(addition);
+    for (let i = 1; i < additionRepeatTimes; i++) {
+      arr.push(additionSeparator);
+      arr.push(addition);
+    }
+    if (y < repeatTimes - 1) {
+      arr.push(separator);
+    }
+  }
+
+  return arr.join("");
+  // console.log(arr.join(""));
 }
 
 module.exports = {
-  repeater
+  repeater,
 };
